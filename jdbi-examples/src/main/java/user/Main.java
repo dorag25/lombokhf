a5a80ea592ea6570fab92844d334aaf2c2093645
dorag25 <44019690+dorag25@users.noreply.github.com>
2020-04-10 20:05:42 +0200

    Add files via upload

---
+++ jdbi-examples/src/main/java/user/Main.java
@@ -0,0 +1,29 @@
+package user;
+
+import jdk.internal.org.jline.reader.History;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test"); // (H2 in-memory database)
+        jdbi.installPlugin(new SqlObjectPlugin());
+        try (Handle handle = jdbi.open()) {
+            UserDao dao = handle.attach(UserDao.class);
+
+            User user = User.builder()
+                    .id(Long)
+                    .username("Pinky")
+                    .password("PinkyMouse")
+                    .name("Miss Pink")
+                    .email("pinky@vmi.hu")
+                    .gender(User.Gender.FEMALE)
+                    .dob(LocalDate.parse("2020.04.10"))
+                    .enabled(true)
+                    .build();
+        }
+    }
+}
