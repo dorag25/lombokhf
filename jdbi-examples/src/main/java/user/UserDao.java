a6cd00001980be79b0d5fc7bf4b9e8a49a87069d
Gabor Dora <g.dora981225@gmail.com>
2020-04-10 20:31:01 +0200

    UserDaou elkezdése

--- jdbi-examples/src/main/java/user/UserDao.java
+++ jdbi-examples/src/main/java/user/UserDao.java
@@ -1,4 +1,23 @@
 package user;
 
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import javax.swing.*;
+import java.sql.Date;
+
+@RegisterBeanMapper(User.class)
 public interface UserDao {
+    @SqlUpdate("""CREATE TABLE user(
+    id IDENTITY PRIMARYKEY,
+    usrename VARCHAR(255),
+    password VARCHAR(255),
+    name VARCHAR(255),
+    email VARCHAR(255),
+    gender Enum("MALE","FEMALE"),
+    dob Date,
+    enabled VARCHAR(255)
+    )"""
+    )
+    void createTable();
 }
